e63fd0c8dc6b86d63c12d7d319f6360aa6c9e953
zlzonkh <zlzonkh1@gmail.com>
2026-02-04 15:07:06 +0900

    feat(enemy): add DummyEnemy concrete script

---
+++ Assets/_Project/Scripts/DummyEnemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class DummyEnemy : EnemyBase
+{
+    // This class currently does not add any new functionality
+    // It simply serves as a concrete implementation of EnemyBase
+}
