bb9afbb40aa4be7967e4fac0561ce5e11568c916
zlzonkh <zlzonkh1@gmail.com>
2026-03-04 13:37:01 +0900

    feat(enemy): define and implement IDamageSource

--- Assets/_Project/Scripts/EnemyBase.cs
+++ Assets/_Project/Scripts/EnemyBase.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
 
-public abstract class EnemyBase : MonoBehaviour, IDamageable
+public abstract class EnemyBase : MonoBehaviour, IDamageable, IDamageSource
 {
     [field: SerializeField] private int _maxHealth;
+    [field: SerializeField] public int _contactDamage;
     private int _currentHealth;
     public int CurrentHealth => _currentHealth;
+    public int DamageAmount => _contactDamage;
 
     void Awake()
     {
---
+++ Assets/_Project/Scripts/IDamageSource.cs
@@ -0,0 +1,4 @@
+public interface IDamageSource
+{
+    int DamageAmount { get; }
+}
