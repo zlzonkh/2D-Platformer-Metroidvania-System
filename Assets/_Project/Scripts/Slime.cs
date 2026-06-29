aa87a2454a5716ef3db006f10b3dc8138c6b247c
zlzonkh <zlzonkh1@gmail.com>
2026-06-30 00:02:17 +0900

    feat(enemy): add Slime concrete class

---
+++ Assets/_Project/Scripts/Slime.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Slime : EnemyBase
+{
+    // This class currently does not add any new functionality
+    // It simply serves as a concrete implementation of EnemyBase
+    // This class will be used to implement a slime enemy that performs simple x-axis movement
+}
